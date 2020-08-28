777a7efdca5944b52de30d816cffd92b22ca2597
bannplayer <bannplayer@gmail.com>
2020-08-28 15:21:01 +0900

    support account address of synapse extension 0.1.0

--- frontend/src/transaction/wallet.js
+++ frontend/src/transaction/wallet.js
@@ -8,24 +8,21 @@ const wallet = {
             throw Error("No wallet found");
         }
 
-        let walletInfo = await ckbInjected.getAddressInfo();
-        if(
-            !(walletInfo.data.address.slice(0, 4) === 'ckt1'
-                && walletInfo.data.type === 'Secp256k1')
-        ) {
-            throw Error("Only Aggron testnet and secp256k1 account available");
-        }
         const ckb = new config.CKB("http://localhost:8114");
-        const pkh = `0x${ckb.utils.blake160(walletInfo.data.publicKey, 'hex')}`;
+
+        let walletInfo = await ckbInjected.getAddressInfo();
+        if(walletInfo.data.type === 'Secp256k1') {
+            throw Error("Only secp256k1 account available");
+        }
 
         return {
             lock : {
-                args: pkh,
+                args: `0x${ckb.utils.blake160(walletInfo.data.publicKey, 'hex')}`,
                 code_hash: "0x9bd7e06f3ecf4be0f2fcd2188b23f1b9fcc88e5d4b65a8637b17723bbda3cce8",
                 hash_type: "type"
             },
             lockHash: walletInfo.data.lock,
-            address : walletInfo.data.address
+            address : ckb.utils.pubkeyToAddress(walletInfo.data.publicKey, {prefix: 'ckt'})
         }
     },
 
